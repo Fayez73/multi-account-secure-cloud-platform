5671bc8fb6b5173c37858b3dd13c26a191458db2
fayez-ukhsa <fayez.ahmed@ukhsa.gov.uk>
2025-08-24 21:03:45 +0100

    logging updated

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -5,13 +5,6 @@ resource "aws_s3_bucket" "org_logs" {
     enabled = true
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   tags = {
     Environment = var.env
